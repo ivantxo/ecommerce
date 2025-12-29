b61c47eb49c6d34541490b46d43c752fd3f5f009
IvanR <ivan.ramirezf@gmail.com>
2025-12-29 23:35:16 +1000

    New server action
    - New server action to fetch latest products
    - Used to load latest products in home page
    - Also added constant for latest products limit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/products.actions";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={14}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
 };
