7222b5fc9dbd0439ad7fa7827eb21b83bf56ba16
IvanR <ivan.ramirezf@gmail.com>
2026-01-02 23:32:09 +1000

    New React ProductDetailsPage
    - Added new Next action: getProductBySlug
    - Added new React page: ProductDetailsPage

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import { getProductBySlug } from "@/lib/actions/products.actions";
+import { notFound } from "next/navigation";
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await props.params;
+
+  const product = await getProductBySlug(slug);
+
+  if (!product) notFound();
+
+  return <>{product.name}</>;
+};
+
+export default ProductDetailsPage;
