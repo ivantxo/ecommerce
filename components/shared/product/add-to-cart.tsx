7eef41d4094cc785c13c3f5fff11a4e85bc705d1
IvanR <ivan.ramirezf@gmail.com>
2026-01-19 23:55:14 +1000

    Cart - Add to cart component

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { CartItem } from "@/types";
+import { useToast } from "@/hooks/use-toast";
+import { Toast, ToastAction } from "@radix-ui/react-toast";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast({
+        variant: "destructive",
+        description: res.message,
+      });
+      return;
+    }
+
+    // Handle success add to cart
+    toast({
+      description: `${item.name} added to cart`,
+      action: (
+        <ToastAction
+          className="bg-slate-300"
+          altText="Go to cart"
+          onClick={() => router.push("/cart")}
+        >
+          Go to cart
+        </ToastAction>
+      ),
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      + Add to Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
