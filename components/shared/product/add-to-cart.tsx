47f5b34a5b95c810f1ef1d13eb0e89d047659bce
Ivan <ivan.ramirezf@gmail>
2026-02-04 23:51:18 +1000

    Cart - Add/Remove from Cart buttons

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
 import { useToast } from "@/hooks/use-toast";
-import { Toast, ToastAction } from "@radix-ui/react-toast";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { ToastAction } from "@radix-ui/react-toast";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -36,12 +36,36 @@ const AddToCart = ({ item }: { item: CartItem }) => {
       ),
     });
   };
+  // Handle remove from cart
+  const handleRemoveFromCart = async () => {
+    const res = await await removeItemFromCart(item.productId);
 
-  return (
+    toast({
+      variant: res?.success ? "default" : "destructive",
+      description: res?.message,
+    });
+
+    return;
+  }
+
+  // Check if item is already in cart
+  const existItem = cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      + Add to Cart
+      <Plus /> Add to Cart
     </Button>
-  );
+  );  
 };
 
 export default AddToCart;
