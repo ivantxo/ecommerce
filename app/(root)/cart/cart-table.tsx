f81d7d767656a193dafd299dd7b06191a3190b3b
Ivan <ivan.ramirezf@gmail>
2026-02-06 22:37:49 +1000

    Cart & Shipping Pages - Subtotal card
    - Subtotal card and checkout button

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -16,6 +16,8 @@ import {
     TableCell,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
 
 const CartTable = ({cart}: {cart?: Cart}) => {
     const router = useRouter();
@@ -84,6 +86,23 @@ const CartTable = ({cart}: {cart?: Cart}) => {
                             </TableBody>
                         </Table>
                     </div>
+
+                    <Card>
+                        <CardContent className="p-4">
+                            <div className="pb-3 text-xl">
+                                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                                <span className="font-bold">{formatCurrency(cart.itemsPrice)}</span>
+                            </div>
+                            <Button className="w-full" disabled={isPending} onClick={() => 
+                                startTransition(() => router.push('/shipping-address'))}>
+                                    {isPending ? (
+                                        <Loader className="animate-spin h-4 w-4" />
+                                    ) : (
+                                        <ArrowRight className="h-4 w-4" />
+                                    )} Proceed to Checkout
+                            </Button>
+                        </CardContent>
+                    </Card>
                 </div>
             )}
         </>
