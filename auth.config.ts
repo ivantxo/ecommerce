e3b20d4d987853a04d06aa4ed50f3efcf20bffe5
Ivan <ivan.ramirezf@gmail>
2026-02-10 01:35:24 +1000

    Cart & Shipping Pages - Protected paths

--- auth.config.ts
+++ auth.config.ts
@@ -4,19 +4,38 @@ import { NextResponse } from "next/server";
 export const authConfig = {
   providers: [],
   callbacks: {
-    authorized({ request, auth }: any) {
+    authorized({ request, auth }) {
+      // Array of reg exp patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from request URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) {
+        // Redirect to sign-in with callback URL
+        return NextResponse.redirect(
+          new URL(`/sign-in?callbackUrl=${encodeURIComponent(pathname)}`, request.url)
+        );
+      }
+
       // Check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie
         const sessionCartId = crypto.randomUUID();
 
-        // Clone req headers
-        const newRequestHeaders = new Headers(request.headers);
-
         // Create new response and add the new headers
         const response = NextResponse.next({
           request: {
-            headers: newRequestHeaders,
+            headers: new Headers(request.headers),
           },
         });
 
