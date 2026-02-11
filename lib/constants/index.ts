8597a6c060ac799cf4ec6a33be97ff2fc4e1c91c
Ivan <ivan.ramirezf@gmail>
2026-02-11 21:40:01 +1000

    Payments & Orders - Constants
    - Added constants: PAYMENT_METHODS and DEFAULT_PAYMENT_METHOD

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -27,3 +27,6 @@ export const shippingAddressDefaultsValues = {
   postalCode: "",
   country: "",
 };
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(", ") : ["PayPal", "Stripe", "CashOnDelivery"];
+export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
