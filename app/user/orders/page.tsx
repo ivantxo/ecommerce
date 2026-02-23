54e1e0ce7cb67c7c6a1cefc73f46c6846d1d568f
Ivan <ivan.ramirezf@gmail>
2026-02-23 22:24:14 +1000

    Removed console.log

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -23,8 +23,6 @@ const OrdersPage = async (props: {
 
   const orders = await getMyOrders({ page: Number(page) || 1 });
 
-  console.log(orders);
-
   return (
     <div className="space-y-2">
       <h2 className="h2-bold">Orders</h2>
