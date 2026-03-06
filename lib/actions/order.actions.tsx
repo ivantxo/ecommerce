639f30ff790b200938214fd57b176acdd8bf0388
Ivan <ivan.ramirezf@gmail>
2026-03-06 23:31:23 +1000

    Fix npm run build
    - Remove unused import from order.actions
    - Next version from 15.2.2 to 16.0.10

--- lib/actions/order.actions.tsx
+++ lib/actions/order.actions.tsx
@@ -10,7 +10,6 @@ import { prisma } from "@/db/prisma";
 import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
-import { format } from "path";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
 
