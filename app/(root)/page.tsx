a8600db8392e34ad2736b4e3630041d32beba163
Ivan <ivan.ramirezf@gmail>
2026-03-11 23:41:41 +1000

    Search, Drawer & Carousel - Carousel III
    - Fixed data type of ProductsCarousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,15 +4,22 @@ import {
   getFeaturedProducts,
 } from "@/lib/actions/products.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
+import { Decimal } from "@prisma/client/runtime/library";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   const featuredProducts = await getFeaturedProducts();
 
+  const formattedFeaturedProducts = featuredProducts.map((product) => ({
+    ...product,
+    price: product.price as unknown as Decimal,
+    rating: product.rating as unknown as Decimal,
+  }));
+
   return (
     <>
       {featuredProducts.length > 0 && (
-        <ProductCarousel data={featuredProducts} />
+        <ProductCarousel data={formattedFeaturedProducts} />
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
