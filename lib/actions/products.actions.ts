d65c93e0685101ea4c9833f75c1f3e5381e81a44
Ivan <ivan.ramirezf@gmail>
2026-03-13 23:08:42 +1000

    Search, Drawer & Carousel - Search Filter

--- lib/actions/products.actions.ts
+++ lib/actions/products.actions.ts
@@ -6,6 +6,7 @@ import { _success } from "zod/v4/core";
 import { revalidatePath } from "next/cache";
 import { insertProductSchema, updateProductSchema } from "../validators";
 import z from "zod";
+import { Prisma } from "@prisma/client";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -51,17 +52,51 @@ export async function getAllProducts({
   rating?: string;
   sort?: string;
 }) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
+  // Category filter
+  const categoryFilter = category && category !== "all" ? { category } : {};
+
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== "all"
+      ? {
+          price: {
+            gte: Number(price.split("-")[0]),
+            lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+
+  // Rating filter
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
+
   const data = await prisma.product.findMany({
-    skip: (page - 1) * limit,
-    take: limit,
     orderBy: { createdAt: "desc" },
     where: {
-      name: {
-        contains: query,
-        mode: "insensitive",
-      },
-      category: category ? category : undefined,
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
     },
+    skip: (page - 1) * limit,
+    take: limit,
   });
 
   const dataCount = await prisma.product.count({
