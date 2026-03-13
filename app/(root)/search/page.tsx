292ea807da70c5e8dcaea2b39e7c6bc0b3d3bfe4
Ivan <ivan.ramirezf@gmail>
2026-03-14 00:38:07 +1000

    Search, Drawer & Carousel - Rating & filters display

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/shared/product/product-card";
+import { Button } from "@/components/ui/button";
 import {
   getAllProducts,
   getAllCategories,
@@ -12,6 +13,8 @@ const prices = [
   { name: "$201 to $500", value: "201 - 500" },
   { name: "$501 to $1000", value: "501 - 500" },
 ];
+
+const ratings = [4, 3, 2, 1];
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -119,8 +122,49 @@ const SearchPage = async (props: {
             ))}
           </ul>
         </div>
+        {/* Rating links */}
+        <div className="text-xl mb2 mt-8">Customer Ratings</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${rating === "all" && "font-bold"}`}
+                href={getFilterUrl({ r: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {ratings.map((r) => (
+              <li key={r}>
+                <Link
+                  className={`${rating === r.toString() && "font-bold"}`}
+                  href={getFilterUrl({ r: `${r}` })}
+                >
+                  {`${r} stars & up`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="md:col-span-4 space-y-4">
+        <div className="flex-between flex-col md:flex-row my-4">
+          <div className="flex items center">
+            {q !== "all" && q !== "" && "Query: " + q}
+            {category !== "all" && category !== "" && " Category: " + category}
+            {price !== "all" && " Price: " + price}
+            {rating !== "all" && " Rating: " + rating + " stars & up"}
+            &nbsp;
+            {(q !== "all" && q !== "") ||
+            (category !== "all" && category !== "") ||
+            rating !== "all" ||
+            price !== "all" ? (
+              <Button variant={"link"} asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+        </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && <div>No products found</div>}
           {products.data.map((product) => (
