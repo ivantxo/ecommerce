bc098890933a15e677abc084de07f09b107ff414
Ivan <ivan.ramirezf@gmail>
2026-03-16 22:54:40 +1000

    Ratings & Reviews - Added types & constants

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,7 @@ import {
   insertOrderItemSchema,
   insertOrderSchema,
   paymentResultSchema,
+  insertReviewSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -27,6 +28,11 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   isDelivered: boolean;
   deliveredAt: Date | null;
   orderitems: OrderItem[];
-  user: {name: string; email: string};
+  user: { name: string; email: string };
 };
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
