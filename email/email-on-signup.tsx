1034f0ad9b07f040690d9ac9246a9ac4e027cd10
Ivan <ivan.ramirezf@gmail>
2026-04-07 00:25:18 +1000

    Resend now instantiated only when the functions are called

--- email/email-on-signup.tsx
+++ email/email-on-signup.tsx
@@ -2,9 +2,8 @@ import { Resend } from "resend";
 import { SENDER_EMAIL, APP_NAME } from "@/lib/constants";
 import SendEmailVerification from "./new-account-confirmation";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const sendEmailVerification = async (email: string, code: string) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
     from: `${APP_NAME} <${SENDER_EMAIL}>`,
     to: email,
